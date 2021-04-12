77e6d698ec26bfa64dc5e35366b39a749e21206f
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-04-12 12:48:10 +0700

    styling and customize for hero component

--- src/parts/Hero.js
+++ src/parts/Hero.js
@@ -19,18 +19,18 @@ export default function Hero(props) {
     <section className="container pt-4">
       <div className="row align-items-center">
         <div className="col-auto pr-5" style={{width: 530}}>
-          <h1 className="h2 font-weight-bold line-height-1 mb-3">
+          <h1 className="font-weight-bold line-height-1 mb-3">
             Forget Busy Work, <br />
             Start Next Vacation
           </h1>
-          <p className="mb-5 font-weigth-light text-gray-500 w-75">
+          <p className="mb-4 font-weigth-light text-gray-500 w-75" style={{lineHeight: "170%"}}>
             We provide what you need to enjoy your holiday with family. Time to 
             make another memorable moments.
           </p>
           <Button className="btn px-5" hasShadow isPrimary onClick={showMostPicked}>
             Show Me Now
           </Button>
-          <div className="row mt-5">
+          <div className="row" style={{marginTop: 80}}>
             <div className="col-auto" style={{marginRight: 35}}>
               <img width="36" height="36" src={IconTraveler} alt={`${props.data.travelers} Travelers`} />
               <h6 className="mt-3">
