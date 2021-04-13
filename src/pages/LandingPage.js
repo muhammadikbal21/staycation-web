517a4070db082ed9806b2d232294fce3464116af
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-04-13 21:57:04 +0700

    add footer component and styling footer component

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -5,6 +5,7 @@ import MostPicked from 'parts/MostPicked'
 import Categories from 'parts/Categories'
 import landingPage from 'json/landingPage.json'
 import Testimony from "parts/Testimony";
+import Footer from "parts/Footer";
 
 export default class LandingPage extends Component {
 
@@ -21,6 +22,7 @@ export default class LandingPage extends Component {
         <MostPicked refMostPicked={this.refMostPicked} data={landingPage.mostPicked} />
         <Categories data={landingPage.categories} />
         <Testimony data={landingPage.testimonial} />
+        <Footer />
       </>
     );
   }
