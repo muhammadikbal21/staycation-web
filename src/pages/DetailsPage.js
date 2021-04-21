2e0a20fd4fa2cecb31fe97e9a7733860b49f68c5
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-04-21 12:17:32 +0700

    add booking form and styling booking form  in DetailsPage.js, and clean code in InputNumber component which  not used

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -3,6 +3,7 @@ import Header from "parts/Header";
 import PageDetailTitle from "parts/PageDetailTitle";
 import FeaturedImage from "parts/FeaturedImage";
 import PageDetailDescription from "parts/PageDetailDescription";
+import BookingForm from "parts/BookingForm";
 import ItemDetails from "json/itemDetails.json"
 
 export default class DetailsPage extends Component {
@@ -29,7 +30,9 @@ export default class DetailsPage extends Component {
             <div className="col-7 pr-5">
               <PageDetailDescription data={ItemDetails} />
             </div>
-            <div className="col-5">Booking Form</div>
+            <div className="col-5">
+              <BookingForm itemDetails={ItemDetails} />
+            </div>
           </div>
         </section>
       </>
